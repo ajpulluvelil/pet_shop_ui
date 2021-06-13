58c9dc14ac33451e964af8566a485f3a30424a4e
Akhil Joseph <ajpulluvelil@gmail.com>
2021-06-13 21:28:41 +0530

    basic structure

--- src/app/login-module/login.module.ts
+++ src/app/login-module/login.module.ts
@@ -5,6 +5,10 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 
+import { MatInputModule } from '@angular/material/input';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -13,7 +17,10 @@ import { RegistrationComponent } from './registration/registration.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatInputModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule
   ]
 })
 export class LoginModule { }
